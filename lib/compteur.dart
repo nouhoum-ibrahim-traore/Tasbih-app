b7c1efe60255e91ba721ae4c304de15fe00c0eea
Nouhoum Ibrahim Traoré <nouhoumit@gmail.com>
2021-06-21 12:10:04 +0000

    This is a tasbih app. Missing: audio

--- lib/compteur.dart
+++ lib/compteur.dart
@@ -3,16 +3,45 @@ import 'package:flutter/material.dart';
 
 class Compteur with ChangeNotifier, DiagnosticableTreeMixin {
   int nombreDeClics = 0;
+  int limite = 33;
+  Color couleur = Colors.blue;
+
+  Color get getCouleur => couleur;
+
   int get count => nombreDeClics;
 
   void incrementer() {
     nombreDeClics++;
+    if (nombreDeClics == 0) {
+      couleur = Colors.blue;
+    }
+
+    ;
+    if (nombreDeClics == limite) {
+      couleur = Colors.yellowAccent;
+      nombreDeClics = 0;
+    }
     notifyListeners();
   }
+
+  void updateLimite(int newLimite) {
+    limite = newLimite;
+    couleur = Colors.blue;
+    notifyListeners();
+  }
+
+  void reset() {
+    nombreDeClics = 0;
+    couleur = Colors.blue;
+    notifyListeners();
+  }
+
+  void changerCouleur() {}
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(IntProperty('count', count));
+    properties.add(ColorProperty('count', getCouleur));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -27,7 +29,10 @@ class NotreAppState extends State<NotreApp> {
 }
 
 class PageDaccueil extends StatelessWidget {
+  static var _audioPlayer = AudioPlayer();
+
   final String title;
+  static var textEditingController = TextEditingController();
 
   const PageDaccueil({Key key, this.title}) : super(key: key);
 
@@ -38,20 +43,89 @@ class PageDaccueil extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => context.read<Compteur>().incrementer(),
+      floatingActionButton: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 130,
+          width: 130,
+          child: FloatingActionButton(
+            backgroundColor: context.watch<Compteur>().getCouleur,
+            child: Icon(Icons.add),
+            onPressed: () => context.read<Compteur>().incrementer(),
+          ),
+        ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Le nombre de fois que vous avez cliqué: '),
-            Text(context.watch<Compteur>().nombreDeClics.toString())
-          ],
-        ),
+        child: ListView(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: textEditingController,
+                      decoration: InputDecoration(hintText: 'Entrer la limite'),
+                    ),
+                  ),
+                  RaisedButton(
+                      color: Colors.blue,
+                      child: Text('Valider'),
+                      onPressed: () => context.read<Compteur>().updateLimite(
+                          int.parse(textEditingController.text.toString()))),
+                ],
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Text('Le nombre de fois que vous avez cliqué: ',
+                  style: TextStyle(
+                    fontSize: 17,
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                context.watch<Compteur>().nombreDeClics.toString(),
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                  // onPressed: () {
+                  //   context.read<Compteur>().reset();
+                  //   playLocalAsset();
+                  // },
+                  onPressed: () async {
+                    //String filePath = await FilePicker.getFilePath();
+
+                    await _audioPlayer.play("mp3Test.mp3", isLocal: true);
+
+                    // if(status == 1){
+                    //   setState(() {
+                    //     isPlaying = true;
+                    //   });
+                    // }
+                  },
+                  child: Icon(Icons.refresh),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                  ))
+            ],
+          ),
+        ]),
       ),
     );
     throw UnimplementedError();
+  }
+
+  Future<AudioPlayer> playLocalAsset() async {
+    AudioCache cache = new AudioCache();
+    //At the next line, DO NOT pass the entire reference such as assets/yes.mp3. This will not work.
+    //Just pass the file name only.
+    return await cache.play("mp3Test.mp3");
   }
 }
